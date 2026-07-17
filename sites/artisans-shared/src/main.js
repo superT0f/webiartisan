a9499d20107bbd9b00fb6bb3fd2fe9014df709a4
SuperT0f <christophe.prigent2@gmail.com>
2026-07-17 16:05:08 +0200

    feat(routes): /carte devient la page par défaut, annuaire déplacé sur /annuaire

--- sites/artisans-shared/src/main.js
+++ sites/artisans-shared/src/main.js
@@ -8,7 +8,8 @@ import { consumeTokenFromQuery } from './auth.js'
 import { useGamification } from './composables/useGamification.js'
 
 const routes = [
-  { path: '/', component: () => import('./views/Home.vue'), meta: { title: 'Artisans de ' + import.meta.env.VITE_CITY_NAME } },
+  { path: '/', redirect: '/carte' },
+  { path: '/annuaire', component: () => import('./views/Home.vue'), meta: { title: 'Artisans de ' + import.meta.env.VITE_CITY_NAME } },
   { path: '/carte', name: 'Map', component: () => import('./views/MapView.vue'), meta: { title: 'Carte des artisans' } },
   { path: '/artisan/:id', component: () => import('./views/Artisan.vue'), meta: { title: 'Fiche artisan' } },
   { path: '/temoignages', name: 'Testimonials', component: () => import('./views/Testimonials.vue'), meta: { title: 'Avis et recommandations locales' } },
@@ -37,13 +38,13 @@ const routes = [
   { path: '/personnage', component: () => import('./views/CharacterEdit.vue'), meta: { title: 'Mon personnage' } },
   { path: '/abonnement/success', component: () => import('./views/SubscriptionSuccess.vue'), meta: { title: 'Abonnement activé' } },
   { path: '/abonnement/cancel', component: () => import('./views/SubscriptionCancel.vue'), meta: { title: 'Paiement annulé' } },
-  { path: '/:pathMatch(.*)*', redirect: '/' }
+  { path: '/:pathMatch(.*)*', redirect: '/carte' }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior: () => ({ top: 0 })
+  scrollBehavior: (to) => (to.hash ? { el: to.hash, behavior: 'smooth' } : { top: 0 })
 })
 
 // Connexion automatique par lien magique : consomme ?token= sur n'importe
